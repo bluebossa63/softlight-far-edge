e9b44df22e3e8c7a305b1ede8800807bd85423ac
Daniele Ulrich <1701938+bluebossa63@users.noreply.github.com>
2021-01-31 14:26:52 +0100

    Update login-vsphere.sh
    path corrected

--- login-vsphere.sh
+++ login-vsphere.sh
@@ -8,8 +8,8 @@ VSPHERE_WITH_TANZU_PASSWORD=$3
 VSPHERE_WITH_TANZU_NAMESPACE=$4
 VSPHERE_WITH_TANZU_CLUSTER=$5
 
-KUBECTL_VSPHERE_PATH=/Users/lamw/Desktop/bin/kubectl-vsphere
-KUBECTL_PATH=/usr/local/bin/kubectl
+KUBECTL_VSPHERE_PATH=/usr/bin/kubectl-vsphere
+KUBECTL_PATH=/usr/bin/kubectl
 
 KUBECTL_VSPHERE_LOGIN_COMMAND=$(expect -c "
 spawn $KUBECTL_VSPHERE_PATH login --server=$VSPHERE_WITH_TANZU_CONTROL_PLANE_IP --vsphere-username $VSPHERE_WITH_TANZU_USERNAME --insecure-skip-tls-verify \
